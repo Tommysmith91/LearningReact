b92075926e02a51d57be4199c66dcdd40d7bb20b
tommy.smith <tommy.smith@cloudcommercegroup.com>
2024-03-11 11:52:36 +0000

    Added some new fields to the create employee form, added way to get to login screen for now

--- src/Pages/CreateEmployeeForm.js
+++ src/Pages/CreateEmployeeForm.js
@@ -6,6 +6,12 @@ export default function CreateEmployeeForm() {
   const formFields = [
     { name: 'name', label: 'Name', type: 'text', placeholder: 'Enter Name' },
     { name: 'age', label: 'Age', type: 'number', placeholder: 'Enter Age' },
+    {name:'addressline1',label:'Address Line 1', type:'text', placeholder: '64 Zoo Lane'},
+    {name:'addressline2',label:'Address Line 2', type:'text',placeholder:'Zoopington'},
+    {name:'city/town',label:'City/Town', type:'text',placeholder: 'Chester'},
+    {name:'country',label:'Country', type:'text',placeholder:'United Kingdom'},
+    {name:'postcode',label:'Postcode',type:'text',placeholder:'TR3 3SS'},
+    {name:'employmentstartdate',label: 'Employment Start Date',type:'date',placeholder: ''},
     {name:'hasrighttowork',label:'Has Right To Work In UK', type:'checkbox'}
    ]
     const onSubmit = (event) => {      
